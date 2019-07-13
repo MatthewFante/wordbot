fd747fc02e379007ece422fb112c84af0336e8c7
Matthew Fante <matthew.fante@gmail.com>
2019-07-12 22:51:42 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$('#wordbotButton').click(function() {
+    let wordbot = new XMLHttpRequest();
+
+    wordbot.onreadystatechange = function() {
+        if(wordbot.readyState === 4) {
+            let response = JSON.parse(wordbot.responseText);
+            let wordbotList = "<div class='wordRow'>";
+            for (i = 0; i < response.words.length; i += 1){
+                if(i % 10 == 0 && i != 0){
+                    wordbotList += "</div><div class='wordRow'>";
+                }
+
+                wordbotList += "<div class='word mx-auto'><a href='https://www.dictionary.com/browse/" + response.words[i] + "' target='_blank'>"+ response.words[i] + "</a></div>";
+
+                console.log(response.words[i]);
+
+            }
+            wordbotList += "</div>";
+            $('#wordbotOutput').html(wordbotList);
+
+
+        };
+    };
+
+    wordbot.open('GET', 'https://api.noopschallenge.com/wordbot?count=' + $('#wordbotCount').val() + '&set=' + $('#wordbotSet').val() );
+    wordbot.send();
+});
