04cb55af1b8a61d49c11d9e861e9939c063870b5
Matthew Fante <matthew.fante@gmail.com>
2019-07-15 19:18:30 -0400

    Added header and removed unused JS

--- js/script.js
+++ js/script.js
@@ -6,22 +6,12 @@ $('#wordbotButton').click(function() {
             let response = JSON.parse(wordbot.responseText);
             let wordbotList = "";
             for (i = 0; i < response.words.length; i += 1){
-                // if(i % 10 == 0 && i != 0){
-                //     wordbotList += "</div><div class='wordRow'>";
-                // }
-
                 wordbotList += "<div class='word mx-auto'><a href='https://www.dictionary.com/browse/" + response.words[i] + "' target='_blank'>"+ response.words[i] + "</a></div>";
-
-                console.log(response.words[i]);
-
+                // console.log(response.words[i]);
             }
-            // wordbotList += "</div>";
             $('#wordbotOutput').html(wordbotList);
-
-
         };
     };
-
     wordbot.open('GET', 'https://api.noopschallenge.com/wordbot?count=' + $('#wordbotCount').val() + '&set=' + $('#wordbotSet').val() );
     wordbot.send();
 });
